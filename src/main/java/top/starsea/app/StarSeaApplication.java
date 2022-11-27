9c503b43d64a3f0372444a321826ffd14ef17b77
XingKong <3301394538@qq.com>
2022-11-27 15:00:54 +0800

    初始化项目

---
+++ src/main/java/top/starsea/app/StarSeaApplication.java
@@ -0,0 +1,17 @@
+package top.starsea.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author XingKong
+ * creationTime: 2022/11/27 14:55
+ */
+@SpringBootApplication
+public class StarSeaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StarSeaApplication.class, args);
+    }
+
+}
